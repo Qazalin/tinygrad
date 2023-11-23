4fa3eeb1c3d266ed1a3c6405fe938cf6373be1f6
qazal <qazal.software@gmail.com>
2023-11-23 22:56:52 +0200

    use render_cast

--- tinygrad/renderer/cstyle.py
+++ tinygrad/renderer/cstyle.py
@@ -100,7 +100,7 @@ class CStyleLanguage(NamedTuple):
     if self.uses_vload and buf_dtype == dtypes.float16 and var_dtype != dtypes.float16:
       return f"vstore_half{'' if var_dtype.sz == 1 else str(var_dtype.sz)}({var_name}, 0, {buf_name}+{idx});"
     if var_dtype.sz > 1:
-      return f"*(({self.smem_prefix if local and self.smem_prefix_for_cast else self.buffer_prefix}{buf_dtype.name}{var_dtype.sz}*)({buf_name}+{idx})) = ({buf_dtype.name}{var_dtype.sz}){var_name};"
+      return f"*(({self.smem_prefix if local and self.smem_prefix_for_cast else self.buffer_prefix}{buf_dtype.vec(var_dtype.sz).name}*)({buf_name}+{idx})) = {self.render_cast([var_name], buf_dtype.vec(var_dtype.sz))};"
     return f"*({buf_name}+{idx}) = {var_name};" if self.uses_ptr_arithmetic else f"{buf_name}[{idx}] = {var_name};"
 
 def uops_to_cstyle(lang:CStyleLanguage, function_name:str, uops:List[UOp]) -> Tuple[str, Dict]:
