3b4bb473d59103c5bea32868a5e08de69b3a9017
qazal <qazal.software@gmail.com>
2023-12-13 02:02:57 +0200

    tests indicating this logic is still broken

--- test/test_lazyop.py
+++ test/test_lazyop.py
@@ -1,11 +1,12 @@
 import unittest
+from hypothesis import given, strategies as st
 from tinygrad.tensor import Tensor
 
 # stuff needed to unpack a kernel
 # ruff: noqa: F401
-from tinygrad.ops import LazyOp, TernaryOps, BinaryOps, UnaryOps, ReduceOps, BufferOps, MemBuffer, ConstBuffer
+from tinygrad.ops import LazyOp, TernaryOps, BinaryOps, UnaryOps, ReduceOps, BufferOps, MemBuffer, ConstBuffer, get_lazyop_info
 from tinygrad.lazy import LazyBuffer
-from tinygrad.helpers import dtypes
+from tinygrad.helpers import DTYPES_DICT, dtypes
 from tinygrad.shape.shapetracker import ShapeTracker
 from tinygrad.shape.view import View
 from tinygrad.shape.symbolic import Variable
@@ -28,6 +29,24 @@ class TestLazyOp(unittest.TestCase):
       for _ in range(i): p = p.e(BinaryOps.ADD, p)
       # sanity check if caching works this should be way faster
       assert time.monotonic() -st < 0.5, f"{i}"
+
+  @given(st.sampled_from([dtype for dtype in DTYPES_DICT.values() if dtypes.is_float(dtype)]))
+  def test_mulacc_fusion(self, d):
+    x = Tensor.rand(1024,1024, dtype=d)
+    w = Tensor.rand(1024,1024, dtype=d)
+    out = (x*w).sum(-1)
+    reduceop = [op for op in out.lazydata.schedule()[-1].ast.get_lazyops() if op.op in ReduceOps][0]
+    assert reduceop.op == ReduceOps.SUM and reduceop.src[0].op == TernaryOps.MULACC and len(reduceop.src[0].src) == 2
+    assert get_lazyop_info(reduceop).dtype == d
+
+  @given(st.sampled_from([dtype for dtype in DTYPES_DICT.values() if dtypes.is_float(dtype)]), st.sampled_from([dtype for dtype in DTYPES_DICT.values() if dtypes.is_float(dtype)]))
+  def test_mulacc_fusion_midcast(self, d1, d2):
+    x = Tensor.rand(1024,1024, dtype=d1)
+    w = Tensor.rand(1024,1024, dtype=d1)
+    out = (x*w).cast(d2).sum(-1)
+    reduceop = [op for op in out.lazydata.schedule()[-1].ast.get_lazyops() if op.op in ReduceOps][0]
+    #assert reduceop.op == ReduceOps.SUM and reduceop.src[0].op == TernaryOps.MULACC and len(reduceop.src[0].src) == 2
+    # assert get_lazyop_info(reduceop).dtype == d2 TODO what do we expect here? max(d1, d2)? d2? d1?
 
 if __name__ == '__main__':
   unittest.main()
