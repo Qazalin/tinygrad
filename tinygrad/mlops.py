f8ddcc8c6d669c21779712ad5bd3d783f99cc800
qazal <qazal.software@gmail.com>
2023-12-17 20:13:22 +0200

    upcast bools to int32 when doing a sum

--- tinygrad/mlops.py
+++ tinygrad/mlops.py
@@ -148,6 +148,7 @@ class Where(Function):
 class Sum(Function):
   def forward(self, x:LazyBuffer, new_shape:Tuple[int, ...]) -> LazyBuffer:
     self.input_shape = x.shape
+    if x.dtype == dtypes.bool: x = x.cast(dtypes.int32)
     return x.r(ReduceOps.SUM, new_shape)
 
   def backward(self, grad_output:LazyBuffer) -> LazyBuffer:
