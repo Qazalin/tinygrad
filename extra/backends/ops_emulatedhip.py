7d9e58175674c275cd30bed65611b0436728127c
qazal <qazal.software@gmail.com>
2024-01-28 18:39:03 +0000

    driver update

--- extra/backends/ops_emulatedhip.py
+++ extra/backends/ops_emulatedhip.py
@@ -1,8 +1,8 @@
-import ctypes, functools
+import ctypes
 from tinygrad.device import Compiled, MallocAllocator
 from tinygrad.renderer.cstyle import HIPRenderer
 from tinygrad.codegen.kernel import LinearizerOptions
-from tinygrad.runtime.ops_hip import compile_hip
+from tinygrad.runtime.ops_hip import HIPCompiler
 
 hip = ctypes.CDLL("/usr/local/lib/libremu.so")
 
@@ -10,11 +10,9 @@ class EmulatedHIPProgram:
   def __init__(self, name:str, lib:bytes):
     self.name, self.lib = name, lib
   def __call__(self, *args, global_size, local_size, vals=(), wait=False):
-    assert len(vals) == 0
+    args = (*args, *vals)
     hip.hipModuleLaunchKernel(self.lib, len(self.lib), *global_size, *local_size, 0, None, None, len(args), (ctypes.c_void_p * len(args))(*[ctypes.cast(x, ctypes.c_void_p) for x in args]))
 
 class EmulatedHIPDevice(Compiled):
   def __init__(self, device=""):
-    self.arch = "gfx1100"
-    super().__init__(MallocAllocator, LinearizerOptions("HIP"), HIPRenderer,
-                     functools.partial(compile_hip,arch=self.arch), f"compile_hip_{self.arch}", functools.partial(EmulatedHIPProgram))
+    super().__init__(device, MallocAllocator, HIPCompiler("gfx1100"), EmulatedHIPProgram)
