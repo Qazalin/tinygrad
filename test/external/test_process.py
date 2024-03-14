69ef5eaaa8676d2c05aae49255e08e25f36d7248
qazal <qazal.software@gmail.com>
2024-03-14 17:23:17 +0200

    process replay

---
+++ test/external/test_process.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+from difflib import unified_diff
+import subprocess, os, time
+from typing import List
+from tinygrad.helpers import colored
+
+def replay_kernels(commit: str) -> List[str]:
+  subprocess.run(['git', 'checkout', commit], check=True)
+  env = os.environ.copy()
+  env["DEBUG"] = "4"
+  env["CI"] = "1"
+  result = subprocess.run(["python3", "-m", "pytest", "test/", "-s"], env=env, text=True, capture_output=True)
+  ret = []
+  for log in result.stdout.splitlines()[6:-4]: # ignore pytest logs
+    if "***" in log: continue # filter out device stats
+    ret.append(log)
+  print(len(ret))
+  return ret
+
+if __name__ == "__main__":
+  subprocess.run(["git", "fetch", "origin", "master"], check=True, text=True)
+  ref_commit = subprocess.run(["git", "rev-parse", "origin/master"], stdout=subprocess.PIPE, check=True, text=True).stdout.strip()
+  curr_commit = subprocess.check_output(["git", "rev-parse", "HEAD"], encoding="utf-8").strip()
+
+  st = time.perf_counter()
+  curr = replay_kernels(curr_commit)
+  ref = replay_kernels(ref_commit)
+  try:
+    assert curr == ref
+    print(colored("TESTS PASSED", "green"))
+  except AssertionError:
+    print(colored("REPLAY TESTS FAILED", "red"))
+    diff = "\n".join(unified_diff(ref, curr, lineterm=""))
+    with open(f"{curr_commit}.diff", "w") as f: f.write(diff)
+  tm = (time.perf_counter() - st) * 100
+  print(f"Replay tests took {tm:6.2f} ms")
