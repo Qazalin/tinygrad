c3e30bdebe0c00de9f8e9e381b9b0c6042e68ef8
qazal <qazal.software@gmail.com>
2024-04-20 14:11:12 +0300

    repro

--- test/models/test_real_world.py
+++ test/models/test_real_world.py
@@ -17,7 +17,7 @@ from examples.stable_diffusion import UNetModel, ResBlock
 global_mem_used = 0
 def helper_test(nm, gen, model, max_memory_allowed, max_kernels_allowed, all_jitted=False):
   tms = []
-  for _ in range(4):
+  for _ in range(2):
     early_gen = [x.realize() if isinstance(x, Tensor) else x for x in gen()]
     GlobalCounters.reset()
     Device[Device.DEFAULT].synchronize()
--- tinygrad/engine/schedule.py
+++ tinygrad/engine/schedule.py
@@ -269,7 +269,9 @@ def create_schedule_with_vars(outs:List[LazyBuffer], seen:Optional[Set[LazyBuffe
       print(f"saving {len(SCHEDULES)} schedule items to", fp:="schedule.pkl")
       pickle.dump(SCHEDULES, open(fp, "wb"))
     if len(SCHEDULES) == 0: atexit.register(_save)
-    SCHEDULES.extend(schedule)
+    if len(outs) == 54:
+      SCHEDULES.clear()
+      SCHEDULES.extend(schedule)
   return schedule, var_vals
 
 def create_schedule(outs:List[LazyBuffer], seen:Optional[Set[LazyBuffer]]=None) -> List[ScheduleItem]:
