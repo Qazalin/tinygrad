268feec0d1c87bd246e8ad1b19b6e6be73df3f41
qazal <qazal.software@gmail.com>
2024-05-18 10:41:56 +0300

    infra 1

--- test/external/fuzz_uops.py
+++ test/external/fuzz_uops.py
@@ -1,11 +1,20 @@
 from collections import deque
 from typing import DefaultDict, List
 from tinygrad.codegen.uops import UOp, UOpGraph
+from tinygrad.device import Device
 
 def fuzz_uops(graph:DefaultDict[UOp, List[UOp]], in_degree:DefaultDict[UOp, int]):
-  variants: List[List[UOp]] = []
-
   # bfs ordering
   queue = deque(x for x, deg in in_degree.items() if deg == 0)
-  print(queue)
+  ret: List[UOp] = []
+  while queue:
+    u = queue.popleft()
+    ret.append(u)
+    for v in graph[u]:
+      in_degree[v] -= 1
+      if in_degree[v] == 0: queue.append(v)
+
+  uops = UOpGraph()
+  uops._uops = ret
+  Device[Device.DEFAULT].renderer.render("variant_0", uops)
   return []
