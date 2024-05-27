cf2d07fd7ecbd3918c4439a4db3834eba7ddd740
qazal <qazal.software@gmail.com>
2024-05-27 11:59:34 +0300

    mood

--- test/external/fuzz_uops.py
+++ test/external/fuzz_uops.py
@@ -1,11 +1,12 @@
-from collections import deque
+from collections import defaultdict, deque
 import numpy as np
 from dataclasses import replace
 from typing import DefaultDict, Dict, List, Set, Tuple
 from tinygrad.codegen.uops import UOp, UOpGraph, UOps
 from tinygrad.device import Buffer, Device
+from tinygrad.engine.graph import save_graph
 from tinygrad.engine.realize import CompiledRunner
-from tinygrad.helpers import DEBUG, colored, getenv
+from tinygrad.helpers import DEBUG, GRAPHPATH, colored, getenv
 from tinygrad.shape.symbolic import Variable
 
 def fuzz_uops(graph:DefaultDict[UOp, List[UOp]], in_degree:DefaultDict[UOp, int], loops_children:Dict[UOp, Set[UOp]]):
@@ -77,8 +78,36 @@ def find_all_toposorts(graph:DefaultDict[UOp, List[UOp]], in_degree:DefaultDict[
   assert len(ret) == len(set(ret))
   return ret
 
+def panic(x=""): raise Exception(x)
+
+def graph_uops(g:DefaultDict[UOp, List[UOp]]):
+  import networkx as nx
+  colors = {UOps.ALU: "#ffffc0", UOps.LOAD: "#ffc0c0", UOps.STORE: "#c0ffc0", UOps.SPECIAL: "#c0c0ff", UOps.CONST: "#e0e0e0",
+            UOps.DEFINE_GLOBAL: "#ffe0b0", UOps.DEFINE_LOCAL: "#ffe0d0", UOps.DEFINE_ACC: "#f0ffe0",
+            UOps.RANGE: "#c8a0e0", UOps.PHI: "#e0ffc0", UOps.BARRIER: "#ff8080", UOps.IF: "#c8b0c0"}
+  G = nx.DiGraph()
+  for u in (uops:=list(g)):
+    G.add_node(uops.index(u), label=f"{str(u.uop)[5:]}{(' '+str(u.arg)) if u.arg is not None else ''}\n{str(u.dtype)}", style="filled", fillcolor=colors.get(u.uop, "#ffffff"))  # noqa: E501
+    for v in g[u]:
+      if v not in uops: continue
+      G.add_edge(uops.index(v), uops.index(u))
+  save_graph(G, f'{GRAPHPATH}.uops', '-Grankdir=LR')
+
 def _bfs(graph:DefaultDict[UOp, List[UOp]], in_degree:DefaultDict[UOp, int]) -> Tuple[UOp, ...]:
+  # *** build the range graph
+  ranges = [x for x in in_degree if x.uop is UOps.RANGE]
+  range_subtrees: DefaultDict[UOp, Set[UOp]] = defaultdict(set)
+  def _get_group(u:UOp, group):
+    if u.uop is UOps.PHI or u in group: return
+    group.add(u)
+    for x in graph[u]: _get_group(x, group)
+  for r in ranges: _get_group(r, range_subtrees[r])
+  # 1. once loop RANGE is inserted, ONLY its children are inserted until you reach END
+  # 2. PHI is always leaf
+
+  # *** sort
   queue = deque(x for x,d in in_degree.items() if d == 0)
+
   ret: List[UOp] = []
   while queue:
     u = queue.popleft()
@@ -87,4 +116,5 @@ def _bfs(graph:DefaultDict[UOp, List[UOp]], in_degree:DefaultDict[UOp, int]) -> 
     for x in graph[u]:
       in_degree[x] -= 1
       if in_degree[x] == 0: queue.append(x)
+
   return tuple(ret)
