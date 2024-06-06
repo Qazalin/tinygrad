0d618c9f30c5cd279c83a67dcaa4b09030d455bc
qazal <qazal.software@gmail.com>
2024-06-06 18:29:18 +0300

    hardcode

--- test/external/replay_codegen.py
+++ test/external/replay_codegen.py
@@ -17,7 +17,21 @@ def get_src(limit:int, offset:int):
   conn.close()
 
 def get_program(branch, limit, offset) -> str:
-  code = inspect.getsource(get_src)+f"\nget_src({limit}, {offset})"
+  code = \
+f"""
+import pickle
+from tinygrad.helpers import db_connection, VERSION
+from tinygrad.codegen.linearizer import Linearizer
+conn = db_connection()
+cur = conn.cursor()
+cur.execute(f"SELECT val FROM 'process_replay_{VERSION}' LIMIT ? OFFSET ?", ({limit}, {offset}))
+for row in cur.fetchall():
+  ast, applied_opts = pickle.loads(row[0])
+  k = Linearizer(*ast)
+  for opt in applied_opts: k.apply_opt(opt)
+  print(k.linearize().to_program().src)
+conn.close()
+"""
   subprocess.run(["git", "checkout", branch], check=True, capture_output=True)
   result = subprocess.run(["python3", "-c", code], capture_output=True, text=True)
   return result.stdout
