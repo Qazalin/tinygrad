58af6238717f47439bc0be6aabd3dd76641fac84
qazal <qazal.software@gmail.com>
2024-06-27 20:16:59 +0300

    do it using replace

--- extra/backends/ops_egg.py
+++ extra/backends/ops_egg.py
@@ -1,21 +1,17 @@
+from dataclasses import replace
 import ctypes, pickle
-from typing import Set
 from tinygrad.codegen.uops import UOp
-from tinygrad.engine.graph import print_tree
 
 lib = ctypes.CDLL("/Users/qazal/code/dev/target/release/libdev.dylib")
 lib.rewrite_uops.argtypes = [ctypes.POINTER(ctypes.c_ubyte), ctypes.c_int]
+
 def egg_graph_rewrite(sink:UOp):
-  print_tree(sink)
-  print("----------")
-  s = pickle.dumps(_fixup_uop(sink, set()))
+  s = pickle.dumps(_fixup_uop(sink))
   byte_array = (ctypes.c_ubyte * len(s)).from_buffer_copy(s)
   lib.rewrite_uops(byte_array, len(s))
 
-def _fixup_uop(uop, seen:Set[UOp]):
-  if uop in seen: return uop
-  seen.add(uop)
-  uop.op = uop.op.name
-  uop.arg, uop.dtype = str(uop.arg) if uop.arg else None, str(uop.dtype) if uop.dtype else None
-  for x in uop.src: _fixup_uop(x, seen)
+def _fixup_uop(uop) -> UOp:
+  uop = replace(uop, op=uop.op.name, arg=_as_str(uop.arg), dtype=_as_str(uop.dtype), src=tuple(_fixup_uop(x) for x in uop.src))
   return uop
+
+def _as_str(x): return str(x) if x is not None else None
