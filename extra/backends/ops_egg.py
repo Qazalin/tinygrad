21b29bc07bda295ecf8357db7a053aa28a469752
qazal <qazal.software@gmail.com>
2024-06-28 23:02:01 +0300

    full

--- extra/backends/ops_egg.py
+++ extra/backends/ops_egg.py
@@ -8,23 +8,24 @@ lib = ctypes.CDLL("/Users/qazal/code/dev/target/release/libdev.dylib")
 lib.rewrite_uops.argtypes = [ctypes.POINTER(ctypes.c_ubyte), ctypes.c_int]
 lib.rewrite_uops.restype = ByteArray
 
-def egg_graph_rewrite(sink:UOp):
+def egg_graph_rewrite(sink:UOp) -> UOp:
   s = pickle.dumps(_serialize(sink))
   byte_array = (ctypes.c_ubyte * len(s)).from_buffer_copy(s)
   ret: ByteArray = lib.rewrite_uops(byte_array, len(s))
   data = ctypes.string_at(ret.ptr, ret.len)
   new_sink = pickle.loads(data)
-  _deserialize(new_sink)
+  new_sink = _deserialize(new_sink)
+  return new_sink
 
 def _serialize(uop) -> UOp:
   uop = replace(uop, op=uop.op.name, arg=_as_str(uop.arg), dtype=_as_str(uop.dtype), src=tuple(_serialize(x) for x in uop.src))
   return uop
 
 def _deserialize(uop_dict):
-  print(uop_dict)
+  #print(uop_dict)
   dt = DTYPES_DICT[uop_dict["dtype"].replace("dtypes.", "")]
   op = UOps[uop_dict["op"]]
-  arg = uop_dict["arg"]
-  print(arg, type(arg))
+  uop = UOp(op, dt, tuple(_deserialize(x) for x in uop_dict["src"]), uop_dict["arg"])
+  return uop
 
 def _as_str(x): return str(x) if x is not None else None
