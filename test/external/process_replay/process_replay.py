e4085652b2cbc58ea8242d1eeda4cb605a074313
qazal <qazal.software@gmail.com>
2024-08-07 00:45:29 +0300

    print error

--- test/external/process_replay/process_replay.py
+++ test/external/process_replay/process_replay.py
@@ -154,6 +154,4 @@ if __name__ == "__main__":
   if SKIP_PROCESS_REPLAY:
     logging.info("skipping process replay.")
     exit(0)
-  try: process_replay()
-  except Exception as e:
-    if ASSERT_DIFF: raise e
+  process_replay()
