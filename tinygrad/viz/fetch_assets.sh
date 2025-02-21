93b062b6d4b4c039136b513ea197f90704887329
qazal <qazal.software@gmail.com>
2025-02-21 22:00:22 +0100

    remove dagre-d3 dependency from viz

--- tinygrad/viz/fetch_assets.sh
+++ tinygrad/viz/fetch_assets.sh
@@ -6,7 +6,7 @@ fetch() {
   curl -o assets/$1 https://$1
 }
 fetch "d3js.org/d3.v5.min.js"
-fetch "dagrejs.github.io/project/dagre-d3/latest/dagre-d3.min.js"
+fetch "dagrejs.github.io/project/dagre/latest/dagre.min.js"
 fetch "cdnjs.cloudflare.com/ajax/libs/highlight.js/11.10.0/styles/default.min.css"
 fetch "cdnjs.cloudflare.com/ajax/libs/highlight.js/11.10.0/highlight.min.js"
 fetch "cdnjs.cloudflare.com/ajax/libs/highlight.js/11.10.0/languages/python.min.js"
--- tinygrad/viz/lib/graph.js
+++ tinygrad/viz/lib/graph.js
@@ -1,3 +1,17 @@
+const canvas = new OffscreenCanvas(0, 0);
+const ctx = canvas.getContext('2d');
+ctx.font = "14px sans-serif";
+
+function getTextDims(text) {
+  let [maxWidth, height] = [0, 0];
+  for (l of text.split("\n")) {
+    const { width, actualBoundingBoxAscent, actualBoundingBoxDescent } = ctx.measureText(l);
+    if (width > maxWidth) maxWidth = width;
+    height += actualBoundingBoxAscent+actualBoundingBoxDescent;
+  }
+  return [maxWidth, height];
+}
+
 function recenterRects(svg, zoom) {
   const svgBounds = svg.node().getBoundingClientRect();
   for (const rect of svg.node().querySelectorAll("rect")) {
@@ -12,12 +26,14 @@ function recenterRects(svg, zoom) {
 
 window.renderGraph = function(graph, additions) {
   // ** initialize the graph
-  const g = new dagreD3.dagre.graphlib.Graph({ compound: true });
+  const g = new dagre.graphlib.Graph({ compound: true });
   g.setGraph({ rankdir: "LR" }).setDefaultEdgeLabel(function() { return {}; });
   g.setNode("addition", {label: "", clusterLabelPos: "top", style: additions.length !== 0 ? "fill: rgba(26, 27, 38, 0.5);" : "display: none;"});
   for (const [k,v] of Object.entries(graph)) {
     const [label, src, color] = v;
-    const node = {label, labelType:"text", style:`fill: ${color};`};
+    // adjust node dims by label dims + add padding
+    const [labelWidth, labelHeight] = getTextDims(label);
+    const node = {label, labelType:"text", style:`fill: ${color};`, width:labelWidth+20, height:labelHeight+30};
     // for PROGRAM UOp we render the node with a code block
     if (label.includes("PROGRAM")) {
       const [name, ...rest] = label.split("\n");
@@ -32,6 +48,9 @@ window.renderGraph = function(graph, additions) {
     if (additions.includes(parseInt(k))) g.setParent(k, "addition");
   }
 
+  // ** calculate layout
+  dagre.layout(g);
+
   // ** select svg render
   const svg = d3.select("#graph-svg");
   const inner = svg.select("g");
@@ -42,7 +61,5 @@ window.renderGraph = function(graph, additions) {
   recenterRects(svg, zoom);
   svg.call(zoom);
 
-  // ** calculate layout + render
-  const render = new dagreD3.render();
-  render(inner, g);
+  // ** render
 }
