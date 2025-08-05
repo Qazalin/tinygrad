ce304913f6cb9d22a2c3fa80bd4ec028816c8810
qazal <qazal.software@gmail.com>
2025-08-05 11:08:27 +0000

    dll fxns

--- t.py
+++ t.py
@@ -12,4 +12,8 @@ if not os.path.exists(fn:=temp("buf.att")):
 
 with open(fn, "rb") as f: raw = f.read()
 lib = ctypes.CDLL("/opt/rocm/lib/librocprof-trace-decoder.so")
-print(lib)
+
+# https://github.com/ROCm/rocprofiler-sdk/blob/fd6f96ffb54054b405a6f05f800c64394126672d/source/lib/rocprofiler-sdk/thread_trace/dl.cpp#L43
+att_parse_data_fn = lib.rocprof_trace_decoder_parse_data
+att_info_fn = lib.rocprof_trace_decoder_get_info_string
+att_status_fn = lib.rocprof_trace_decoder_get_status_string
