aa5d0867ceb18799ed3a9dc7a9c3ff7475fb1670
qazal <qazal.software@gmail.com>
2025-08-05 17:42:10 +0000

    more work

--- t.py
+++ t.py
@@ -1,72 +1,14 @@
 import ctypes, pickle, contextlib, io, enum
-from typing import Any
 from tinygrad.helpers import temp
 from tinygrad.runtime.ops_amd import ProfileSQTTEvent
 from tinygrad.device import Device, ProfileProgramEvent
 from tinygrad.viz.sqtt_parser import Wave, RecordType, PC, TraceData, att_parse_data_fn, se_data_callback_t, trace_callback_t, isa_callback_t
-from tinygrad.viz.sqtt_parser import InstCategory, Inst, TRACE_DECODER_STATUS_SUCCESS, TRACE_DECODER_STATUS_ERROR_OUT_OF_RESOURCES
-
-# taken from https://github.com/ROCm/rocprofiler-sdk/blob/fd6f96ffb54054b405a6f05f800c64394126672d/source/lib/rocprofiler-sdk/thread_trace/decode.cpp#L162C64-L162C72
-# TODO: chunk copy
-def copy_trace_data(buffer:ctypes.POINTER(ctypes.POINTER(ctypes.c_uint8)), buffer_size:ctypes.POINTER(ctypes.c_uint64), userdata:ctypes.c_void_p) -> int:
-  data:TraceData = ctypes.cast(userdata, ctypes.POINTER(TraceData)).contents
-  buffer_size[0] = ret = data.size
-  buffer[0] = data.data
-  data.size = 0
-  return ret
-
-instruction_timing:dict[int, Inst] = {}
-def trace_callback(record_type, events_ptr, size, _) -> int:
-  if size == 0 or not events_ptr: return 0
-  if record_type == RecordType.WAVE:
-    for i in range(size):
-      wave = ctypes.cast(events_ptr, ctypes.POINTER(Wave))[i]
-      for j in range(wave.instructions_size):
-        inst = wave.instructions_array[j]
-        instruction_timing[inst.pc.addr] = inst
-  return 0
-
-# map PC values to instructions
-_isa_map: dict[int, str] = {}
-def get_instruction_at(pc:int) -> str|None:
-  if not _isa_map:
-    programs = [e for e in profile if isinstance(e, ProfileProgramEvent) and e.device.startswith("AMD")]
-    # TODO: exctract pc values by calling LLVM/MC directly.
-    for e in programs:
-      with contextlib.redirect_stdout(buf:=io.StringIO()): Device[e.device].compiler.disassemble(e.lib)
-      for line in buf.getvalue().splitlines()[6:]:
-        if not (line:=line.strip()): continue
-        instr, rest = line.split("//")
-        relative_pc = int(rest.split(":")[0], 16)
-        _isa_map[e.base+relative_pc] = instr.strip()
-  return _isa_map.get(pc)
-
-def isa_callback(instr_ptr, mem_size_ptr, size_ptr, pc:PC, _) -> int:
-  isa = get_instruction_at(pc.addr) or "<missing>" # TODO: this solves itself when size is real
-  isa_bytes = isa.encode("utf-8")
-  max_len = size_ptr[0]
-  if len(isa_bytes) + 1 > max_len:
-    size_ptr[0] = len(isa_bytes) + 1  # include null terminator
-    return TRACE_DECODER_STATUS_ERROR_OUT_OF_RESOURCES
-  ctypes.memmove(instr_ptr, isa_bytes, len(isa_bytes))
-  instr_ptr[len(isa_bytes)] = 0  # null-terminate
-  size_ptr[0] = len(isa_bytes) + 1
-  mem_size_ptr[0] = 4 # TODO: real size
-  return TRACE_DECODER_STATUS_SUCCESS
-
-def sqtt_parse(profile) -> dict[int, Inst]:
-  if not instruction_timing:
-    se_blobs = [e.blob for e in profile if isinstance(e, ProfileSQTTEvent)]
-    raw_data = b"".join(se_blobs)
-    data_buf = ctypes.create_string_buffer(raw_data)
-    userdata = TraceData(ctypes.cast(data_buf, ctypes.POINTER(ctypes.c_uint8)), len(raw_data))
-    att_parse_data_fn(se_data_callback_t(copy_trace_data), trace_callback_t(trace_callback), isa_callback_t(isa_callback), ctypes.pointer(userdata))
-  return instruction_timing
+from tinygrad.viz.sqtt_parser_types import InstCategory
+from tinygrad.viz.sqtt_parser import sqtt_parse, isa_map
 
 if __name__ == "__main__":
   with open(temp("profile.pkl", append_user=True), "rb") as f: profile = pickle.load(f)
-  sqtt_parse(profile)
-  for pc,inst in instruction_timing.items():
-    isa = get_instruction_at(pc)
+  for pc,inst in sqtt_parse(profile).items():
+    isa = isa_map[pc]
     cat = InstCategory(inst.category).name if inst.category in InstCategory._value2member_map_ else f"UNKNOWN({inst.category})"
     print(f"PC=0x{pc:012x} {isa:<50} time={inst.time}, duration={inst.duration}, stall={inst.stall}, category={cat:<8}")
--- tinygrad/viz/sqtt_parser.py
+++ tinygrad/viz/sqtt_parser.py
@@ -1,64 +1,61 @@
-# references:
-# https://github.com/ROCm/rocprofiler-sdk/blob/fd6f96ffb54054b405a6f05f800c64394126672d/source/include/rocprofiler-sdk/experimental/thread-trace/trace_decoder_types.h
-# https://github.com/ROCm/rocprofiler-sdk/blob/fd6f96ffb54054b405a6f05f800c64394126672d/source/lib/rocprofiler-sdk/thread_trace/trace_decoder_api.h
-import ctypes, enum
+import ctypes, contextlib, io
+from tinygrad.runtime.ops_amd import ProfileSQTTEvent
+from tinygrad.helpers import unwrap
+from tinygrad.device import Device, ProfileProgramEvent
+from tinygrad.viz.sqtt_parser_types import Wave, RecordType, PC, TraceData, att_parse_data_fn, se_data_callback_t, trace_callback_t, isa_callback_t
+from tinygrad.viz.sqtt_parser_types import Inst, TRACE_DECODER_STATUS_SUCCESS, TRACE_DECODER_STATUS_ERROR_OUT_OF_RESOURCES
 
-lib = ctypes.CDLL("/opt/rocm/lib/librocprof-trace-decoder.so")
-att_parse_data_fn = lib.rocprof_trace_decoder_parse_data
-se_data_callback_t = ctypes.CFUNCTYPE(ctypes.c_uint64, ctypes.POINTER(ctypes.POINTER(ctypes.c_uint8)), ctypes.POINTER(ctypes.c_uint64), ctypes.c_void_p)
-trace_callback_t = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_int, ctypes.c_void_p, ctypes.c_uint64, ctypes.c_void_p)
+# taken from https://github.com/ROCm/rocprofiler-sdk/blob/fd6f96ffb54054b405a6f05f800c64394126672d/source/lib/rocprofiler-sdk/thread_trace/decode.cpp#L162C64-L162C72
+# TODO: implement chunk copy
+def copy_trace_data(buf, buf_size, userdata) -> int:
+  data = ctypes.cast(userdata, ctypes.POINTER(TraceData)).contents
+  buf_size[0] = ret = data.size
+  buf[0] = data.data
+  data.size = 0
+  return ret
 
-class PC(ctypes.Structure):
-  _fields_ = [("addr", ctypes.c_size_t), ("marker_id", ctypes.c_size_t)]
+_instruction_timing:dict[int, Inst] = {}
+def trace_callback(record_type, events_ptr, size, _) -> int:
+  if size == 0 or not events_ptr: return 0
+  if record_type == RecordType.WAVE:
+    for i in range(size):
+      wave = ctypes.cast(events_ptr, ctypes.POINTER(Wave))[i]
+      for j in range(wave.instructions_size):
+        inst = wave.instructions_array[j]
+        _instruction_timing[inst.pc.addr] = inst
+  return 0
 
-class Inst(ctypes.Structure):
-  _fields_ = [
-    ("category", ctypes.c_uint32, 8),
-    ("stall", ctypes.c_uint32, 24),
-    ("duration", ctypes.c_int32),
-    ("time", ctypes.c_int64),
-    ("pc", PC)
-  ]
+# map PC values to instructions
+isa_map:dict[int, str] = {}
+def isa_callback(instr_ptr, mem_size_ptr, size_ptr, pc:PC, _) -> int:
+  isa = isa_map.get(pc.addr, "<missing>") # TODO: this solves itself when size is real
+  isa_bytes = isa.encode("utf-8")
+  max_len = size_ptr[0]
+  if len(isa_bytes) + 1 > max_len:
+    size_ptr[0] = len(isa_bytes) + 1  # include null terminator
+    return TRACE_DECODER_STATUS_ERROR_OUT_OF_RESOURCES
+  ctypes.memmove(instr_ptr, isa_bytes, len(isa_bytes))
+  instr_ptr[len(isa_bytes)] = 0  # null-terminate
+  size_ptr[0] = len(isa_bytes) + 1
+  mem_size_ptr[0] = 4 # TODO: real size
+  return TRACE_DECODER_STATUS_SUCCESS
 
-class RecordType(enum.IntEnum): WAVE = 3
-
-class Wave(ctypes.Structure):
-  _fields_ = [
-    ("cu", ctypes.c_uint8),
-    ("simd", ctypes.c_uint8),
-    ("wave_id", ctypes.c_uint8),
-    ("contexts", ctypes.c_uint8),
-    ("_rsvd1", ctypes.c_uint32),
-    ("_rsvd2", ctypes.c_uint32),
-    ("_rsvd3", ctypes.c_uint32),
-    ("begin_time", ctypes.c_int64),
-    ("end_time", ctypes.c_int64),
-    ("timeline_size", ctypes.c_size_t),
-    ("instructions_size", ctypes.c_size_t),
-    ("timeline_array", ctypes.c_void_p),
-    ("instructions_array", ctypes.POINTER(Inst))
-  ]
-
-class InstCategory(enum.IntEnum):
-  NONE = 0
-  SMEM = 1
-  SALU = 2
-  VMEM = 3
-  FLAT = 4
-  LDS = 5
-  VALU = 6
-  JUMP = 7
-  NEXT = 8
-  IMMED = 9
-  CONTEXT = 10
-  MESSAGE = 11
-  BVH = 12
-  LAST = 13
-
-class TraceData(ctypes.Structure):
-  _fields_ = [("data", ctypes.POINTER(ctypes.c_uint8)), ("size", ctypes.c_uint64),]
-
-isa_callback_t = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.POINTER(ctypes.c_char), ctypes.POINTER(ctypes.c_uint64), ctypes.POINTER(ctypes.c_uint64), PC, ctypes.c_void_p)
-
-TRACE_DECODER_STATUS_SUCCESS = 0
-TRACE_DECODER_STATUS_ERROR_OUT_OF_RESOURCES = 4
+def sqtt_parse(profile) -> dict[int, Inst]:
+  if not _instruction_timing:
+    # load programs
+    programs = [e for e in profile if isinstance(e, ProfileProgramEvent) and e.device.startswith("AMD")]
+    # TODO: exctract pc values by calling LLVM/MC directly
+    for e in programs:
+      with contextlib.redirect_stdout(buf:=io.StringIO()): Device[e.device].compiler.disassemble(unwrap(e.lib))
+      for line in buf.getvalue().splitlines()[6:]:
+        if not (line:=line.strip()): continue
+        instr, rest = line.split("//")
+        relative_pc = int(rest.split(":")[0], 16)
+        isa_map[unwrap(e.base)+relative_pc] = instr.strip()
+    # load SQTT buffers
+    se_blobs = [e.blob for e in profile if isinstance(e, ProfileSQTTEvent)]
+    raw_data = b"".join(se_blobs)
+    data_buf = ctypes.create_string_buffer(raw_data)
+    userdata = TraceData(ctypes.cast(data_buf, ctypes.POINTER(ctypes.c_uint8)), len(raw_data))
+    att_parse_data_fn(se_data_callback_t(copy_trace_data), trace_callback_t(trace_callback), isa_callback_t(isa_callback), ctypes.pointer(userdata))
+  return _instruction_timing
