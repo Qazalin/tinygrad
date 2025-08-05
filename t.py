63363f74b75729529113e58ba3e3583d837b2790
qazal <qazal.software@gmail.com>
2025-08-05 11:06:44 +0000

    cdll /opt/rocm/lib/librocprof-trace-decoder.so

--- t.py
+++ t.py
@@ -1,14 +1,15 @@
-import pickle, subprocess, sys, os
+import pickle, subprocess, sys, os, ctypes
 from tinygrad.runtime.ops_amd import ProfileSQTTEvent
 from tinygrad.device import ProfileProgramEvent
 from tinygrad.helpers import getenv, temp
 
-with open(temp("profile.pkl", append_user=True), "rb") as f: profile = pickle.load(f)
-for e in profile:
-  if isinstance(e, ProfileProgramEvent):
-    byte_len = (e.base.bit_length()+7)//8
-    le_bytes = e.base.to_bytes(byte_len, byteorder="little")
-    print(e.name, e.base)
-  if isinstance(e, ProfileSQTTEvent):
-    #subprocess.run("xxd", input=e.blob, stdout=open(f"sqtt/{e.se}.hex", "w"), check=True)
-    print(len(e.blob))
+if not os.path.exists(fn:=temp("buf.att")):
+  with open(temp("profile.pkl", append_user=True), "rb") as f: profile = pickle.load(f)
+  for e in profile:
+    if isinstance(e, ProfileSQTTEvent):
+      with open(fn, "wb") as f: f.write(e.blob)
+      break
+
+with open(fn, "rb") as f: raw = f.read()
+lib = ctypes.CDLL("/opt/rocm/lib/librocprof-trace-decoder.so")
+print(lib)
