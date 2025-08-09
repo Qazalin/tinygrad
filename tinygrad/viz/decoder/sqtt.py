3909e344e128d115d699d9994a912843c70b1900
qazal <qazal.software@gmail.com>
2025-08-09 10:29:01 +0000

    work

--- tinygrad/viz/decoder/sqtt.py
+++ tinygrad/viz/decoder/sqtt.py
@@ -16,13 +16,31 @@ class RecordType(enum.IntEnum): GFXIP = 0; OCCUPANCY = 1; WAVE = 3; INFO = 4 # n
 
 PC = init_c_struct_t((("addr", ctypes.c_size_t), ("marker_id", ctypes.c_size_t)))
 
+# Describes an instruction execution event.
+# The duration is measured as stall+issue time (gfx9) or stall+execution time (gfx10+).
+# Time + duration marks the issue (gfx9) or execution (gfx10+) completion time.
+# Time + stall marks the successful issue time.
+# Duration - stall is the issue time (gfx9) or execution time (gfx10+).
 Inst = init_c_struct_t((("category", ctypes.c_uint32, 8), ("stall", ctypes.c_uint32, 24), ("duration", ctypes.c_int32), ("time", ctypes.c_int64),
                         ("pc", PC)))
 
+# instructions_array contains a time-ordered list of all (traced) instructions by the wave.
 Wave = init_c_struct_t((("cu", ctypes.c_uint8), ("simd", ctypes.c_uint8), ("wave_id", ctypes.c_uint8), ("contexts", ctypes.c_uint8),
                         ("_rsvd1", ctypes.c_uint32), ("_rsvd2", ctypes.c_uint32), ("_rsvd3", ctypes.c_uint32), ("begin_time", ctypes.c_int64),
                         ("end_time", ctypes.c_int64), ("timeline_size", ctypes.c_size_t), ("instructions_size", ctypes.c_size_t),
                         ("timeline_array", ctypes.c_void_p), ("instructions_array", ctypes.POINTER(Inst))))
+
+# Describes an occupancy event (wave started or wave ended).
+Occupancy = init_c_struct_t((
+  ("pc", PC),                    # Wave start address (kernel entry point)
+  ("time", ctypes.c_uint64),     # timestamp of the event
+  ("reserved", ctypes.c_uint8),
+  ("cu", ctypes.c_uint8),        # Compute unit ID (gfx9) or WGP ID (gfx10+)
+  ("simd", ctypes.c_uint8),      # SIMD ID [0,3] within compute unit
+  ("slot", ctypes.c_uint8),      # Wave slot ID within SIMD
+  ("start", ctypes.c_uint32, 1), # 1 if wave_start, 0 if a wave_end
+  ("_rsvd", ctypes.c_uint32, 31)
+))
 
 class DecoderStatus(enum.IntEnum): SUCCESS = 0; OUT_OF_RESOURCES = 2; INVALID_ARG = 3 # noqa: E702
 
@@ -30,23 +48,30 @@ class DecoderStatus(enum.IntEnum): SUCCESS = 0; OUT_OF_RESOURCES = 2; INVALID_AR
 
 @ctypes.CFUNCTYPE(ctypes.c_uint64, ctypes.POINTER(ctypes.POINTER(ctypes.c_uint8)), ctypes.POINTER(ctypes.c_uint64), ctypes.POINTER(TraceData))
 def copy_cb(buf, buf_size, data_ptr):
-  if DEBUG >= 2: print("[copy_cb] {buf_size[0]} {data.size}")
   data = data_ptr.contents
+  if DEBUG >= 2: print(f"[copy_cb] size={buf_size[0]} remaining={data.sz}")
   buf[0] = data.buf_ptr
   buf_size[0] = copied_sz = data.sz
   data.data = ctypes.cast(ctypes.addressof(data.buf_ptr.contents), ctypes.POINTER(ctypes.c_uint8))
   data.sz = 0
   return copied_sz
 
+def decode_occupancy(events:ctypes.POINTER(Occupancy), n:int, data:TraceData):
+  print(f"Parsing {n//2} wavefronts")
+  print(events[0].time)
+
 @ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_int, ctypes.c_void_p, ctypes.c_uint64, ctypes.POINTER(TraceData))
 def trace_cb(record_type, events_ptr, n, data_ptr):
   if DEBUG >= 2: print(f"[trace_cb] {RecordType(record_type).name} {n}")
+  if record_type == RecordType.GFXIP: print(f"Decoding SQTT trace for gfx{events_ptr}")
+  if record_type == RecordType.OCCUPANCY: decode_occupancy(ctypes.cast(events_ptr, ctypes.POINTER(Occupancy)), n, data_ptr.contents)
   return DecoderStatus.SUCCESS
 
 @ctypes.CFUNCTYPE(ctypes.c_int, ctypes.POINTER(ctypes.c_char), ctypes.POINTER(ctypes.c_uint64), ctypes.POINTER(ctypes.c_uint64), PC,
                   ctypes.POINTER(TraceData))
 def isa_cb(instr_ptr, mem_size_ptr, size_ptr, pc, data_ptr):
   if DEBUG >= 2: print(f"[isa_cb] {pc.addr} {pc.marker_id}")
+  raise KeyboardInterrupt
   return DecoderStatus.SUCCESS
 
 # ** main loop
