7c545e81c6be534d5ba39fd7955d724c61d81edc
qazal <qazal.software@gmail.com>
2025-10-18 19:45:04 +0800

    start with profile_matches

--- tinygrad/uop/ops.py
+++ tinygrad/uop/ops.py
@@ -975,19 +975,22 @@ def track_rewrites(name:Callable[..., str|TracingKey]|bool=True, replay:bool=Fal
   return _decorator
 
 active_rewrites:list[TrackedGraphRewrite] = []
-def track_matches(func):
-  def _track_func(*args, **kwargs):
-    if tracking:=(TRACK_MATCH_STATS >= 2):
-      loc = ((frm:=sys._getframe(1)).f_code.co_filename, frm.f_lineno)
-      depth = len(active_rewrites)
-      if not tracked_ctxs: add_trace_group(TracingKey(f"default {func.__name__}"))
-      tracked_ctxs[-1].append(ctx:=TrackedGraphRewrite(loc, args[0].trace_num, [], kwargs.get("name", None), depth, kwargs.get("bottom_up", False)))
-      active_rewrites.append(ctx)
-    with cpu_profile(kwargs.get("name", "<unnamed>"), "TINY", display=tracking):
-      ret = func(*args, **kwargs)
-    if tracking: active_rewrites.pop()
-    return ret
-  return _track_func
+def profile_matches(name_arg:str|None=None):
+  def dec(func):
+    def _track_func(*args, **kwargs):
+      name = kwargs.get("name", name_arg)
+      if tracking:=(TRACK_MATCH_STATS >= 2):
+        loc = ((frm:=sys._getframe(1)).f_code.co_filename, frm.f_lineno)
+        depth = len(active_rewrites)
+        if not tracked_ctxs: add_trace_group(TracingKey(f"default {func.__name__}"))
+        tracked_ctxs[-1].append(ctx:=TrackedGraphRewrite(loc, args[0].trace_num, [], name, depth, kwargs.get("bottom_up", False)))
+        active_rewrites.append(ctx)
+      with cpu_profile(name or "<unnamed>", "TINY", display=tracking):
+        ret = func(*args, **kwargs)
+      if tracking: active_rewrites.pop()
+      return ret
+    return _track_func
+  return dec
 
 class TrackedPatternMatcher(PatternMatcher):
   def rewrite(self, uop:UOp, ctx=None) -> UOp|None:
@@ -1127,12 +1130,12 @@ class RewriteContext:
           self.replace[n] = replaced_new_n
     return self.replace[root]
 
-@track_matches
+@profile_matches()
 def graph_rewrite(sink:UOp, pm:PatternMatcher, ctx=None, bottom_up=False, name=None, bpm=None) -> UOp:
   rewrite_ctx = RewriteContext(pm if not bottom_up else None, pm if bottom_up else bpm, ctx)
   return rewrite_ctx.unified_rewrite(sink)
 
-@track_matches
+@profile_matches()
 def graph_rewrite_map(sink:UOp, pm:PatternMatcher, ctx=None, bottom_up=False, name=None, bpm=None,
                       input_map:dict[UOp, UOp]|None=None, ) -> dict[UOp, UOp]:
   rewrite_ctx = RewriteContext(pm if not bottom_up else None, pm if bottom_up else bpm, ctx)
