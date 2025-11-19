28890e6f71ae7d9e170ec64ffa058117c35cfffc
qazal <qazal.software@gmail.com>
2025-11-19 19:31:00 +0200

    work

--- t2.py
+++ t2.py
@@ -1,8 +1,31 @@
-from tinygrad.helpers import system
-from tinygrad import Device
+from tinygrad import Device, Tensor
+from tinygrad.device import Buffer
+from tinygrad.engine.realize import get_program, CompiledRunner
+from tinygrad.helpers import getenv
 
-dev = Device["AMD"]
+Device.DEFAULT = "AMD"
+dev = Device[Device.DEFAULT]
 
-with open("/tmp/test", "rb") as f: lib = f.read()
-ret = system("llvm-objdump -dr --no-show-raw-insn --triple=amdgcn-amd-amdhsa --mcpu=gfx1100 -", input=lib)
-with open("/tmp/test.s", "w") as f: f.write(ret)
+import subprocess
+if getenv("MOCKGPU"): subprocess.run(["cargo", "build", "--release", "--manifest-path", "./extra/remu/Cargo.toml"], check=True)
+
+with open("test.s", "r") as f: src = f.read()
+lib = dev.compiler.compile(src)
+
+out = Tensor([1.,2,3]).realize() + Tensor([4.,5,6]).realize()
+si = out.schedule()[-1]
+si.bufs[0].allocate()
+prg = get_program(si.ast, dev.renderer)
+fn = CompiledRunner(prg, lib)
+fn(si.bufs, wait=True)
+print(si.bufs[0].numpy())
+
+# like validate_with_cpu
+cpu = Device["CPU"]
+cpu_bufs = [Buffer("CPU", b.size, b.dtype) for b in si.bufs]
+for cpu_b, gpu_b in zip(cpu_bufs, si.bufs):
+  if gpu_b.is_allocated(): cpu_b.ensure_allocated().copyin(gpu_b.as_buffer())
+cpu_fn = CompiledRunner(get_program(si.ast, cpu.renderer))
+cpu_fn(cpu_bufs, wait=True)
+import numpy as np
+np.testing.assert_allclose(si.bufs[0].numpy(), cpu_bufs[0].numpy(), rtol=1e-3, atol=1e-3)
