196f7dd6936c361982bbc4f628ef1d48e346a59b
qazal <qazal.software@gmail.com>
2025-12-03 19:21:25 +0200

    work

--- extra/sqtt/pmc/examples/k0_matrix_add_2d_v2.c
+++ extra/sqtt/pmc/examples/k0_matrix_add_2d_v2.c
@@ -1,6 +1,6 @@
 __global__ void matrix_add_2d(const arr_t * __restrict__ A, const arr_t * __restrict__ B, arr_t * __restrict__ C, const size_t sw, const size_t sh){
-  size_t idx = __ockl_get_local_id(1) + __ockl_get_local_size(1) * (size_t)__ockl_get_group_id(1);
-  size_t idy = __ockl_get_local_id(0) + __ockl_get_local_size(0) * (size_t)__ockl_get_group_id(0);
+  size_t idx = __ockl_get_local_id(1) * (size_t)__ockl_get_group_id(1);
+  size_t idy = __ockl_get_local_id(0) * (size_t)__ockl_get_group_id(0);
   int sw = 1024;
   int sh = 1024;
   if ((idx < sh) && (idy < sw)) {
