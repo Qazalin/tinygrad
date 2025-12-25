497f6a95e25691c218ed859f8bca15d5e06570b6
qazal <qazal.software@gmail.com>
2025-12-25 01:38:59 -0800

    tgemm and exit

--- extra/gemm/asm/test.py
+++ extra/gemm/asm/test.py
@@ -17,11 +17,12 @@ B = (torch.randn(N, N, dtype=torch.float32, device="cpu") / scale).to(torch.bflo
 Bt = B.t().contiguous() # transpose B for the baseline gemm
 C_torch = A@Bt
 
-if getenv("EGEMM"):
+if getenv("TGEMM"):
   from aiter.tuned_gemm import tgemm
   gpu = "cuda:0"
   C_aiter = tgemm.mm(A.to(gpu), B.to(gpu), None, None, None)
   assert torch.allclose(C_aiter.to("cpu"), C_torch, rtol=1e-2, atol=1e-3)
+  exit(0)
 
 # ** copy buffers to AMD
 
