4bea64c0c08cc6f0063de168da5d7a99bd7e0535
qazal <qazal.software@gmail.com>
2026-01-01 13:07:25 +0000

    work

--- extra/gemm/asm/rdna3/test.py
+++ extra/gemm/asm/rdna3/test.py
@@ -5,6 +5,8 @@ from tinygrad.uop.ops import UOp, Ops, KernelInfo
 
 from extra.gemm.amd_uop_matmul import test_matmul
 
+from tinygrad.helpers import getenv
+
 M = N = K = 4096
 TM = TN = 96
 THREADS_PER_WG = 128
@@ -21,7 +23,11 @@ def asm_kernel() -> UOp:
   b = UOp.placeholder((N*N,), dtypes.half, slot=2)
   c = UOp.placeholder((N*N,), dtypes.half, slot=0)
 
-  src = template.replace("INSTRUCTIONS", (pathlib.Path(__file__).parent/"gemm.s").read_text())
+  if getenv("DSL"):
+    from extra.gemm.asm.rdna3.gemm import asm
+    src = "\n".join([s if isinstance(s, str) else s.disasm() for s in asm])
+  else: src = (pathlib.Path(__file__).parent/"gemm.s").read_text()
+  src = template.replace("INSTRUCTIONS", src)
 
   sink = UOp.sink(a, b, c, lidx, gidx, arg=KernelInfo(name="gemm"))
   return UOp(Ops.PROGRAM, src=(sink, UOp(Ops.DEVICE, arg=dname), UOp(Ops.LINEAR, src=(*sink.src, sink)), UOp(Ops.SOURCE, arg=src)))
