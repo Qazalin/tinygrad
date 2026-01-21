b9c86ff4763f0e22f0b03a9d341d3d5ed2aa1d10
qazal <qazal.software@gmail.com>
2026-01-20 20:04:37 -0800

    custom_kernel assembly

---
+++ t.sh
@@ -0,0 +1,5 @@
+B=6 PROFILE=0 LD_PRELOAD=$PWD/libhiptrace.so python t.py
+B=16 PROFILE=0 LD_PRELOAD=$PWD/libhiptrace.so python t.py
+B=32 PROFILE=0 LD_PRELOAD=$PWD/libhiptrace.so python t.py
+B=64 PROFILE=0 LD_PRELOAD=$PWD/libhiptrace.so python t.py
+B=72 PROFILE=0 LD_PRELOAD=$PWD/libhiptrace.so python t.py
