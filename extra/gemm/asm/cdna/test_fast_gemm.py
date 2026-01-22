3876cc09251960108299ca96812667747e1231c3
qazal <qazal.software@gmail.com>
2026-01-21 23:45:49 -0800

    work

--- extra/gemm/asm/cdna/test_fast_gemm.py
+++ extra/gemm/asm/cdna/test_fast_gemm.py
@@ -2,6 +2,7 @@ import unittest
 import numpy as np
 from extra.gemm.asm.cdna.fast_gemm import fast_gemm
 from tinygrad import Tensor, dtypes, Context
+from tinygrad.engine.jit import TinyJit
 
 def test_gemm(A_shape, B_shape):
   rng = np.random.default_rng(1337)
@@ -119,6 +120,58 @@ class TestGemm(unittest.TestCase):
 
   def test_gpt2_alt(self):
     test_gemm((1, 13, 1024), (1024, 3072))
+
+  @unittest.skip("backward not implemented")
+  def test_backward(self):
+    rng = np.random.default_rng(1337)
+    A_np = rng.random((4, 256, 256), dtype=np.float32) - 0.5
+    B_np = rng.random((256, 256), dtype=np.float32) - 0.5
+
+    A = Tensor(A_np, dtype=dtypes.half, requires_grad=True)
+    B = Tensor(B_np, dtype=dtypes.half, requires_grad=True)
+    with Context(DEBUG=0): Tensor.realize(A, B)
+    C_asm = fast_gemm(A, B)
+    C_asm.sum().backward()
+    grad_A_asm, grad_B_asm = A.grad, B.grad
+    Tensor.realize(C_asm, grad_A_asm, grad_B_asm)
+
+    A_ref = Tensor(A_np, dtype=dtypes.half, requires_grad=True)
+    B_ref = Tensor(B_np, dtype=dtypes.half, requires_grad=True)
+    with Context(DEBUG=0): Tensor.realize(A_ref, B_ref)
+    C_ref = A_ref @ B_ref
+    C_ref.sum().backward()
+    grad_A_ref, grad_B_ref = A_ref.grad, B_ref.grad
+    Tensor.realize(C_ref, grad_A_ref, grad_B_ref)
+
+    np.testing.assert_allclose(C_asm.numpy(), C_ref.numpy(), rtol=1e-2, atol=1e-2)
+    np.testing.assert_allclose(grad_A_asm.numpy(), grad_A_ref.numpy(), rtol=1e-2, atol=1e-2)
+    np.testing.assert_allclose(grad_B_asm.numpy(), grad_B_ref.numpy(), rtol=1e-2, atol=1e-2)
+
+  @unittest.skip("multi not implemented")
+  def test_multi(self):
+    devs = ("AMD:0", "AMD:1")
+    rng = np.random.default_rng(1337)
+    A = Tensor(rng.random((4, 256, 256), dtype=np.float32) - 0.5, dtype=dtypes.half).shard_(devs, axis=0)
+    B = Tensor(rng.random((256, 256), dtype=np.float32) - 0.5, dtype=dtypes.half).to(devs)
+    with Context(DEBUG=0): Tensor.realize(A, B)
+    C_asm = fast_gemm(A, B)
+    C_tiny = A @ B
+    Tensor.realize(C_asm, C_tiny)
+    np.testing.assert_allclose(C_asm.numpy(), C_tiny.numpy(), rtol=1e-2, atol=1e-2)
+
+  def test_jit(self):
+    @TinyJit
+    def jit_gemm(A, B): return fast_gemm(A, B).realize()
+
+    rng = np.random.default_rng(1337)
+    for _ in range(5):
+      A = Tensor(rng.random((4, 256, 256), dtype=np.float32) - 0.5, dtype=dtypes.half)
+      B = Tensor(rng.random((256, 256), dtype=np.float32) - 0.5, dtype=dtypes.half)
+      with Context(DEBUG=0): Tensor.realize(A, B)
+      C_asm = jit_gemm(A, B)
+      C_tiny = A @ B
+      Tensor.realize(C_tiny)
+      np.testing.assert_allclose(C_asm.numpy(), C_tiny.numpy(), rtol=1e-2, atol=1e-2)
 
 if __name__ == "__main__":
   unittest.main()
