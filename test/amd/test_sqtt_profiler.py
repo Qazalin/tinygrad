68a7ffd07aa3b8360f7b36430bebe989d2868ac5
qazal <qazal.software@gmail.com>
2026-02-21 19:49:40 +0800

    simple tests

---
+++ test/amd/test_sqtt_profiler.py
@@ -0,0 +1,38 @@
+import unittest, contextlib
+from tinygrad import Device, Tensor, Context
+from tinygrad.viz.serve import load_amd_counters
+
+@contextlib.contextmanager
+def save_sqtt():
+  Device[Device.DEFAULT].synchronize()
+  Device[Device.DEFAULT].profile_events.clear()
+  yield (ret:=[])
+  load_amd_counters(ret, Device[Device.DEFAULT].profile_events)
+  ret[:] = [r for r in ret if r["name"].startswith("Exec")]
+
+@unittest.skipUnless(Device.DEFAULT == "AMD", "only runs on AMD")
+class TestSQTTProfiler(unittest.TestCase):
+  @classmethod
+  def setUpClass(cls):
+    if not Device[Device.DEFAULT].sqtt_enabled: raise unittest.SkipTest("requires device to be in SQTT profiling mode")
+
+  def test_simple(self):
+    t = Tensor.empty(1) + 1
+    with save_sqtt() as sqtt:
+      ei = t.schedule()[0].lower()
+      ei.run()
+    self.assertEqual(len(sqtt), 1)
+    self.assertEqual(sqtt[0]["name"], f"Exec {ei.prg.p.function_name}")
+
+  def test_multiple_runs(self):
+    t = Tensor.empty(1) + 1
+    with save_sqtt() as sqtt:
+      ei = t.schedule()[0].lower()
+      for _ in range(N:=3):
+        ei.run()
+    self.assertEqual(len(sqtt), N)
+    for i in range(1, N):
+      self.assertEqual(sqtt[i]["name"], f"Exec {ei.prg.p.function_name} n{i+1}")
+
+if __name__ == "__main__":
+  unittest.main()
