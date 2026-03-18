4e22562103f12855b839c1e0dff6c750101078c6
qazal <qazal.software@gmail.com>
2026-03-18 17:51:48 +0000

    repl

---
+++ test/amd/test_sqtt_model2.py
@@ -0,0 +1,35 @@
+import os
+os.environ["VIZ"] = "-2"
+import unittest
+from extra.gemm.amd_asm_matmul import Kernel
+from tinygrad.runtime.autogen.amd.rdna3.ins import *
+from tinygrad.uop.ops import UOp, Ops, KernelInfo
+from tinygrad import Tensor, Device
+
+def asm_fxn(*args:tuple[UOp, ...], k:Kernel, lx=1, gx=1, name:str="asm_fxn") -> UOp:
+  lidx = UOp.special(lx, "lidx0")
+  gidx = UOp.special(gx, "gidx0")
+  sink = UOp.sink(*[t.base for t in args], lidx, gidx, arg=KernelInfo(name=name))
+  return UOp(Ops.PROGRAM, src=(sink, UOp(Ops.DEVICE, arg="AMD"), UOp(Ops.LINEAR, src=tuple([UOp(Ops.INS, arg=x) for x in k.finalize()]))))
+
+def load_sqtt() -> bytes:
+  for e in Device[Device.DEFAULT].profile_events:
+    if type(e).__name__ == "ProfileSQTTEvent" and e.se == 1: return e.blob
+
+class TestSQTTModel(unittest.TestCase):
+  def setUp(self):
+    self.arch = getattr(Device[Device.DEFAULT].renderer, "arch", "")
+    if not self.arch.startswith("gfx11"): self.skipTest("only rdna3")
+
+  def test_nop(self):
+    k = Kernel(self.arch)
+    k.emit(s_nop(0))
+    k.emit(s_endpgm())
+    a = Tensor.empty(1)
+    a = a.custom_kernel(a, fxn=functools.partial(asm_fxn, k=k))[0]
+    a.realize()
+    sqtt = load_sqtt()
+    print(sqtt)
+
+if __name__ == "__main__":
+  unittest.main()
