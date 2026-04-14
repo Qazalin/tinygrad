a2d9d4a9b80fcd6ff30f3e2fc4886ab6998c8e2b
qazal <qazal.software@gmail.com>
2026-04-14 06:08:16 -0700

    all zero as expected

--- extra/thunder/amd/quantize_fp8.py
+++ extra/thunder/amd/quantize_fp8.py
@@ -1,5 +1,9 @@
-from tinygrad import Tensor
+from tinygrad import Tensor, dtypes
 
 def custom_quantize_fp8(x:Tensor, amax_state:Tensor|None=None) -> None:
   assert isinstance(x.device, str), "multi todo"
   assert amax_state is None, "todo"
+  x_fp8 = Tensor.invalid(*x.shape, dtype=dtypes.fp8e4m3, device=x.device)
+  inv_scale = Tensor.invalid(1, dtype=dtypes.float, device=x.device).reshape(())
+  new_amax = Tensor.invalid(1, dtype=x.dtype, device=x.device).reshape(())
+  return x_fp8, inv_scale, new_amax
