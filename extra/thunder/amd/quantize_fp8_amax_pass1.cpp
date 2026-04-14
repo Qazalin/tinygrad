f3c412b846a409446a93677f7e59af7fc3f987dc
qazal <qazal.software@gmail.com>
2026-04-14 07:19:05 -0700

    start work

--- extra/thunder/amd/quantize_fp8_amax_pass1.cpp
+++ extra/thunder/amd/quantize_fp8_amax_pass1.cpp
@@ -1,13 +1,6 @@
 #include "kittens.cuh"
-#include <math.h>
 using namespace kittens;
 
 extern "C" __global__ void custom_quantize_fp8_amax_pass1(float *partial_amax_ptr, const bf16 *x_ptr) {
-  if (threadIdx.x != 0) return;
-  float local_max = 0.0f;
-  for (int i = blockIdx.x; i < NUMEL; i += gridDim.x) {
-    float v = fabsf(float(x_ptr[i]));
-    local_max = fmaxf(local_max, v);
-  }
-  partial_amax_ptr[blockIdx.x] = local_max;
+
 }
--- extra/thunder/amd/quantize_fp8_amax_pass2.cpp
+++ extra/thunder/amd/quantize_fp8_amax_pass2.cpp
@@ -1,10 +1,5 @@
 #include "kittens.cuh"
-#include <math.h>
 using namespace kittens;
 
 extern "C" __global__ void custom_quantize_fp8_amax_pass2(float *amax_ptr, const float *partial_amax_ptr) {
-  if (blockIdx.x != 0 || threadIdx.x != 0) return;
-  float amax = 0.0f;
-  for (int i = 0; i < PARTIAL_NUMEL; i++) amax = fmaxf(amax, partial_amax_ptr[i]);
-  amax_ptr[0] = amax;
 }
