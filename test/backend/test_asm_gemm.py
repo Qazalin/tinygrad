8f06cbe49c5e2c872946ea1629bf3957a751f0dc
qazal <qazal.software@gmail.com>
2026-09-06 04:18:01 +0000

    rand inputs

--- test/backend/test_asm_gemm.py
+++ test/backend/test_asm_gemm.py
@@ -186,9 +186,10 @@ class TestMXFP4(unittest.TestCase):
 
   def test_empty(self):
     M, N, K = getenv("M", 16384), getenv("N", 4096), getenv("K", 14336)
-    a = Tensor.empty(M, K, dtype=dtypes.bfloat16)
-    b = Tensor.empty(N, K, dtype=dtypes.bfloat16)
-    for _ in range(getenv("CNT", 1)): asm_gemm(a, b.T, mxfp4=True).realize()
+    a = Tensor.rand(M, K, dtype=dtypes.bfloat16)
+    b = Tensor.rand(N, K, dtype=dtypes.bfloat16)
+    with Context(DEBUG=0, PROFILE=0): Tensor.realize(a, b)
+    for _ in range(getenv("CNT", 1)): out = asm_gemm(a, b.T, mxfp4=True).realize()
 
 # test the Asm GEMM with Llama shapes, only run on the real machine for speed
 
